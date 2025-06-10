41c88a69d321b638e223a23b066e491b00228e6a
AaliyaSheikhTekit <aaliya.s@tekitsolution.com>
2025-06-10 10:52:09 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,234 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Finexity",
+    des: "Built a React.js and Next.js based platform for secure financial asset management with a scalable UI.",
+    img: "/finexity.svg",
+    iconLists: ["/re.svg", "/next.svg"],
+    link: "https://finexity.com", // adjust to real URL or route
+  },
+  {
+    id: 2,
+    title: "Datumsurge",
+    des: "Developed a robust web platform using Next.js, Tailwind, MUI, AWS, and TypeScript.",
+    img: "/datumsurge.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/mui.svg", "/aws.svg", "/ts.svg"],
+    link: "https://dmcs.datumsurge.com/",
+  },
+  {
+    id: 3,
+    title: "Cureme",
+    des: "Migrated legacy React app to Next.js with improved SEO and styling using JS and SASS.",
+    img: "/cureme.svg",
+    iconLists: ["/next.svg", "/js.svg", "/sass.svg"],
+    link: "https://cureme.at",
+  },
+  {
+    id: 4,
+    title: "Hireway",
+    des: "End-to-end platform using Next.js, Checkr, Veriff, Google Maps, Radix UI, PWA, and styled-components.",
+    img: "/hireway.svg",
+    iconLists: [
+      "/next.svg",
+      "/checkr.svg",
+      "/veriff.svg",
+      "/radix.svg",
+      "/ts.svg",
+    ],
+    link: "https://hireway.com",
+  },
+  {
+    id: 5,
+    title: "Amigo AI",
+    des: "Built a collaborative AI platform using React-Remix, WebSocket, Vite.js, and TypeScript.",
+    img: "/amigoai.svg",
+    iconLists: ["/remix.svg", "/ts.svg", "/vite.svg", "/ws.svg"],
+    link: "https://amigoai.es",
+  },
+  {
+    id: 6,
+    title: "GustoPOS",
+    des: "Developed a responsive POS system with MUI, TypeScript, and Next.js.",
+    img: "/gustopos.svg",
+    iconLists: ["/next.svg", "/ts.svg", "/mui.svg"],
+    link: "https://gustopos.com",
+  },
+  {
+    id: 7,
+    title: "WarelyPOS",
+    des: "Led HTML-to-Next.js migration to modernize the legacy POS interface.",
+    img: "/warelypos.svg",
+    iconLists: ["/html.svg", "/next.svg"],
+    link: "https://warelypos.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Software Engineer II - Encoresky Technologies",
+    desc: "Implemented front-end architecture with React.js & Next.js, boosted SEO and performance. Led HTML-to-Next.js migrations and integrated AWS services including S3 & Cognito with DynamoDB.",
+    className: "md:col-span-2",
+    thumbnail: "/encoresky.jpg",
+  },
+  {
+    id: 2,
+    title: "React JS Developer - Sebone Technologies",
+    desc: "Developed responsive UIs with React.js, optimized checkout processes for e-commerce, and enhanced performance & SEO using Tailwind CSS and MUI.",
+    className: "md:col-span-2",
+    thumbnail: "/sebone.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/aaliya-sheikh-242417202/",
+  },
+];
